d7bf4772f575136172f94cb9d50b0dc982fd84c0
François-Pierre <francoispierrerousseau.44@gmail.com>
2023-11-29 11:11:04 +0100

    add test to openapi package

--- packages/api-core/src/request/params/decorators/params.decorator.ts
+++ packages/api-core/src/request/params/decorators/params.decorator.ts
@@ -1,42 +1,64 @@
-import {values} from "lodash";
-import 'reflect-metadata'
-import {RequestCore, TypesCore} from "api-core-types";
+import { values } from "lodash";
+import { RequestCore, TypesCore } from "api-core-types";
 import * as expressValidators from "express-validator";
 
-
 export abstract class ParamsDecorator<
-    TParam extends RequestCore.Params.ParamType,
-    TConstructor extends TypesCore.New = TypesCore.New
-> implements RequestCore.Params.IParamsDecorator<TParam> {
-    public metadata: Record<number, RequestCore.Params.Param<TParam> & { index: number }> // Utiliser un tableau et non un record
+  TParam extends RequestCore.Params.ParamType,
+  TConstructor extends TypesCore.New = TypesCore.New,
+> implements RequestCore.Params.IParamsDecorator<TParam>
+{
+  public metadata: Record<
+    number,
+    RequestCore.Params.Param<TParam> & { index: number }
+  >; // Utiliser un tableau et non un record
 
-    // A typer
-    protected readonly types: TConstructor[]
+  // A typer
+  protected readonly types: TConstructor[];
 
-    protected constructor(
-        public readonly metadataKey: 'body' | 'services' | 'params' | 'map' | 'query',
-        protected readonly target: Object,
-        protected readonly methodName: string | symbol
-    ) {
-        this.metadata = Reflect.getMetadata(this.metadataKey, target, methodName) ?? {}
-        this.types = Reflect.getMetadata('design:paramtypes', target, methodName)
-    }
+  protected constructor(
+    public readonly metadataKey:
+      | "body"
+      | "services"
+      | "params"
+      | "map"
+      | "query",
+    protected readonly target: Object,
+    protected readonly methodName: string | symbol,
+  ) {
+    this.metadata =
+      Reflect.getMetadata(this.metadataKey, target, methodName) ?? {};
 
+    this.types = Reflect.getMetadata("design:paramtypes", target, methodName);
+  }
 
-    abstract add(index: number, option?: { required?: boolean, type?: TParam, name?: string }): void
+  abstract add(
+    index: number,
+    option?: { required?: boolean; type?: TParam; name?: string },
+  ): void;
 
+  get values(): (RequestCore.Params.Param<TParam> & { index: number })[] {
+    return values(this.metadata);
+  }
 
-    get values(): (RequestCore.Params.Param<TParam> & { index: number })[] {
-        return values(this.metadata)
-    }
+  protected addParameter(
+    index: number,
+    type: TParam,
+    name: string,
+    required?: boolean,
+  ): void {
+    this.metadata[index] = { index, type, name, required };
+    Reflect.defineMetadata(
+      this.metadataKey,
+      this.metadata,
+      this.target,
+      this.methodName,
+    );
+  }
 
-    protected addParameter(index: number, type: TParam, name: string, required?: boolean): void {
-        this.metadata[index] = { index, type, name, required };
-        Reflect.defineMetadata(this.metadataKey, this.metadata, this.target, this.methodName);
-    }
-
-    // Indiquer qu'il peut être undefenid ! important !
-    getParam(index: number): RequestCore.Params.Param<TParam> & { index: number } {
-        return this.metadata[index]
-    }
+  // Indiquer qu'il peut être undefenid ! important !
+  getParam(
+    index: number,
+  ): RequestCore.Params.Param<TParam> & { index: number } {
+    return this.metadata[index];
+  }
 }
--- packages/api-core/src/request/params/decorators/params.path.decorator.ts
+++ packages/api-core/src/request/params/decorators/params.path.decorator.ts
@@ -1,45 +1,60 @@
-import {ParamsDecorator} from "./params.decorator";
-import 'reflect-metadata'
-import {RequestCore} from "api-core-types";
+import { ParamsDecorator } from "./params.decorator";
+import "reflect-metadata";
+import { RequestCore } from "api-core-types";
 
-export class ParamsPathDecorator extends ParamsDecorator<
+export class ParamsPathDecorator
+  extends ParamsDecorator<
     RequestCore.Params.ParamPathType,
     NumberConstructor | StringConstructor
-> implements RequestCore.Params.IParamsPathDecorator{
-    constructor(
-        protected readonly target: Object,
-        protected readonly methodName: string | symbol
+  >
+  implements RequestCore.Params.IParamsPathDecorator
+{
+  constructor(
+    protected readonly target: Object,
+    protected readonly methodName: string | symbol,
+  ) {
+    super("params", target, methodName);
+  }
+
+  override add(
+    index: number,
+    option: { name: string; type?: "int" | "float" },
+  ): void {
+    const typeConstruct = this.types[index];
+
+    if (typeConstruct !== Number && typeConstruct !== String) {
+      throw new Error(
+        `Invalid parameter type for '${option.name}' in method '${
+          this.methodName as string
+        }'. The parameter should be a Number or a String, but a ${
+          typeConstruct.name
+        } was provided.`,
+      );
+    }
+
+    if (
+      typeConstruct === String &&
+      (option?.type === "int" || option?.type === "float")
     ) {
-        super('params', target, methodName);
+      throw new Error(
+        `Invalid parameter type for '${option.name}' in method '${
+          this.methodName as string
+        }'. The parameter should be a Number, but a ${
+          typeConstruct.name
+        } was provided. 'int' or 'float' can only be associated with a 'Number' type for this specific parameter.`,
+      );
     }
 
-    override add(index: number, option: { name: string, type?: 'int' | 'float' }): void {
-        const typeConstruct = this.types[index]
-
-        if (typeConstruct !== Number && typeConstruct !== String) {
-            throw new Error(`Invalid parameter type for '${option.name}' in method '${this.methodName as string}'. The parameter should be a Number or a String, but a ${typeConstruct.name} was provided.`)
-        }
-
-        if (typeConstruct === String && (option?.type === 'int' || option?.type === 'float')) {
-            throw new Error(`Invalid parameter type for '${option.name}' in method '${this.methodName as string}'. The parameter should be a Number, but a ${typeConstruct.name} was provided. 'int' or 'float' can only be associated with a 'Number' type for this specific parameter.`);
-        }
-
-
-        const type = option?.type ?? typeConstruct.name.toLowerCase() as RequestCore.Params.ParamPathType
-        super.addParameter(index, type, option.name)
-    }
+    const type =
+      option?.type ??
+      (typeConstruct.name.toLowerCase() as RequestCore.Params.ParamPathType);
+    super.addParameter(index, type, option.name);
+  }
 }
 
-export function Params(
-    paramName: string,
-    type?: 'int' | 'float'
-) {
-    return (
-        target: Object,
-        methodName: string,
-        parameterIndex: number
-    ) => {
-        const paramsPath = new ParamsPathDecorator(target, methodName)
-        paramsPath.add(parameterIndex, {name: paramName, type})
-    }
+export function Params(paramName: string, type?: "int" | "float") {
+  return (target: Object, methodName: string, parameterIndex: number) => {
+    const paramsPath = new ParamsPathDecorator(target, methodName);
+    paramsPath.add(parameterIndex, { name: paramName, type });
+  };
 }
